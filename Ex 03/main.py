2ca698897e532cfb21f0bfba6245daf9611ba381
YuriSilvaW <yuri@nexti.com>
2021-08-13 22:59:00 -0300

    ajustado ex03

--- Ex 03/main.py
+++ Ex 03/main.py
@@ -1,11 +1,16 @@
-L = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+notas = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-quantidadeDeValores = sum(L)
+soma_das_notas = 0
 
-media = quantidadeDeValores/20
+for nota in notas:
+    soma_das_notas += nota
+
+print(soma_das_notas)
+
+media = soma_das_notas/20
 
 print("A média é de:", media)
 
-print('A valor máximo é:', max(L))
+print('A valor máximo é:', max(notas))
 
-print('A valor mínimo é:', min(L))
+print('A valor mínimo é:', min(notas))
